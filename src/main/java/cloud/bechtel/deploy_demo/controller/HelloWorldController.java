7e40226879349e13c3bd1bdba7b61c9c124c8a31
Kay Bechtel <kay.bechtel@nl-it-consulting.de>
2026-06-05 13:34:28 +0200

    first commit

---
+++ src/main/java/cloud/bechtel/deploy_demo/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package cloud.bechtel.deploy_demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello world";
+    }
+}
---
+++ src/test/java/cloud/bechtel/deploy_demo/DeployDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.bechtel.deploy_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
